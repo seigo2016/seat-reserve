6586fd8de725f8551b06b849cea6e13f9b6afff2
Ricotan <shunsuke_18n1100908@nnn.ed.jp>
2019-06-13 07:42:50 +0100

    reset

--- main.py
+++ main.py
@@ -5,33 +5,44 @@ import time
 import signal
 import sys
 import random
-import ConfigParser
-from slackclient import SlackClient
 
-def post(sc):
-    print sc.api_call(
-            "chat.postMessage", channel="#なぼへXXXする", text="<@XXX> かわいいよなぼちゃん♡",
-            username='りこたそ', icon_emoji=':なぼたん:'
-            )
-
-    def handler(signum, frame):
+def handler(signum, frame):
         print 'Signal handler called with signal',signum
-    GPIO.cleanup()
-    sys.exit(0)
+        GPIO.cleanup()
+        sys.exit(0)
+
+l = list(range(1, 31))
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.IN)
 before = 0
 
-config = ConfigParser.ConfigParser()
-config.read('config.ini')
- 
-token = config.get('development', 'token')
-sc = SlackClient(token)
-
-now = GPIO.input(9)
-if before == 0 and now == 1:
-      aikatsu = ['軍艦島座席番号1','軍艦島座席番号2','軍艦島座席番号3','軍艦島座席番号4','軍艦島座席番号5','軍艦島座席番号6','屋久島座席番号1','屋久島座席番号2','屋久島座席番号3','屋久島座席番号4','屋久島座席番号5','屋久島座席番号6','小豆島座席番号1','小豆島座席番号2','小豆島座席番号3','小豆島座席番号4','小豆島座席番号5','小豆島座席番号6','種子島座席番号1','種子島座席番号2','種子島座席番号3','種子島座席番号4','種子島座席番号5','種子島座席番号6','佐渡島座席番号1','佐渡島座席番号2','佐渡島座席番号3','佐渡島座席番号4','佐渡島座席番号5','佐渡島座席番号6']
-      print(random.choice(aikatsu))
-      time.sleep(0.1)
-before = now
+while True:
+    now = GPIO.input(9)
+    if before == 0 and now == 1:
+        number = random.choice(l)
+        #print("ahoshine")
+        if number <= 6:
+        	print("軍艦島座席番号")
+        	print(number)
+        elif 7 <= number <= 12:
+        	print("屋久島座席番号")
+        	print(number)
+        elif 13 <= number <= 18:
+        	print("小豆島座席番号")
+            print(number)
+        elif 19 <= number <= 24:
+        	print("佐渡島座席番号")
+        	print(number)
+        elif 25 <= number <= 30:
+        	print("種子島座席番号")
+        	print(number))
+        # elif number == XX
+             # print(椅子が壊れてるよ、引き直してね。)
+        time.sleep(0.1)
+        l.remove(number)
+        if not l:
+            print("座席は全て埋まりました。空いているお席にどうぞ。")
+            sys.exit()
+    time.sleep(0.1)
+    before = now
